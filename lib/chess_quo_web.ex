47e9804bd860e1d9c0ca18a5145af5ffcc3c4f3c
matthew-hajec <matthajec@pm.me>
2025-08-19 22:42:47 -0400

    added chess_board component

--- lib/chess_quo_web.ex
+++ lib/chess_quo_web.ex
@@ -86,6 +86,8 @@ defmodule ChessQuoWeb do
       import Phoenix.HTML
       # Core UI components
       import ChessQuoWeb.CoreComponents
+      # Chess Components
+      import ChessQuoWeb.ChessComponents
 
       # Common modules used in templates
       alias Phoenix.LiveView.JS
