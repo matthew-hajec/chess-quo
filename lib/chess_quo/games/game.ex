4e09a05edb1939a67e93b0a9dd2eee942d4e840b
matthew-hajec <matthajec@pm.me>
2025-08-21 10:24:47 -0400

    add Game context and migration for games table

---
+++ lib/chess_quo/games/game.ex
@@ -0,0 +1,34 @@
+defmodule ChessQuo.Game do
+  @moduledoc """
+  The Game context.
+  """
+
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "games" do
+    field :code, :string
+    field :white_secret, :string # Secret shared with the white player to verify their moves
+    field :black_secret, :string # Secret shared with the black player to verify their moves
+    field :turn, :string, default: "white"
+    field :board, :map, default: %{}
+    field :state, :string, default: "waiting" # Possible states: waiting, playing, finished
+    field :winner, :string, default: nil # Possible values: "white", "black", or nil if no winner yet
+    field :white_joined, :boolean, default: false
+    field :black_joined, :boolean, default: false
+    field :moves, {:array, :map}, default: []
+    field :started_at, :utc_datetime # This is the time when the game actually started (not record creation time, this is when "state" becomes "playing")
+    field :lock_version, :integer, default: 0 # Used for optimistic locking
+
+    timestamps()
+  end
+
+  @doc false
+  def system_changeset(game, attrs) do
+    game
+    |> change(attrs)
+    |> cast(attrs, [:code, :white_secret, :black_secret, :turn, :board, :state, :winner, :white_joined, :black_joined, :moves, :started_at])
+    |> validate_required([:code, :white_secret, :black_secret, :turn, :board, :state, :white_joined, :black_joined, :moves])
+    |> optimistic_lock(:lock_version)
+  end
+end
---
+++ priv/repo/migrations/20250821140751_create_games.exs
@@ -0,0 +1,24 @@
+defmodule ChessQuo.Repo.Migrations.CreateGames do
+  use Ecto.Migration
+
+  def change do
+    create table(:games) do
+      add :code, :string, null: false
+      add :white_secret, :string
+      add :black_secret, :string
+      add :turn, :string, default: "white"
+      add :board, :map, default: %{}
+      add :state, :string, default: "waiting"
+      add :winner, :string
+      add :white_joined, :boolean, default: false
+      add :black_joined, :boolean, default: false
+      add :moves, {:array, :map}, default: []
+      add :started_at, :utc_datetime
+      add :lock_version, :integer, default: 0
+
+      timestamps()
+    end
+
+    create unique_index(:games, [:code])
+  end
+end
