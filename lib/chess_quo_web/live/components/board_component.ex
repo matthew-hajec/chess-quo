0efaf4edc5fd13862cd4b24413db80eaa13b6add
matthew-hajec <matthajec@pm.me>
2025-08-22 16:54:47 -0400

    update BoardComponent to use dynamic perspective

--- lib/chess_quo_web/live/components/board_component.ex
+++ lib/chess_quo_web/live/components/board_component.ex
@@ -2,13 +2,17 @@ defmodule ChessQuoWeb.BoardComponent do
   use ChessQuoWeb, :live_component
 
   def render(assigns) do
+    # Index 0 = a1, index 1 = b1... index 8 = a2
     ~H"""
     <div class="w-full mx-auto">
       <div class="grid grid-cols-8 gap-0 aspect-square w-full border-2 border-gray-800">
-        <%= for rank <- 8..1//-1 do %>
-          <%= for file <- ?a..?h do %>
+        <% rank_range = if @perspective == "white" do 1..8 else 8..1//-1 end %>
+        <% file_range = if @perspective == "white" do ?a..?h else ?h..?a//-1 end %>
+
+        <%= for rank <- rank_range do %>
+          <%= for file <- file_range do %>
             <% square = <<file>> <> Integer.to_string(rank) %>
-            <% is_light_square = rem(file - ?a + rank, 2) == 0 %>
+            <% is_light_square = rem(file - ?a + rank, 2) == 1 %>
             <div
               id={square}
               class={[
