0c0b455af1ed647e6625e4210c4a7089e68725f6
matthew-hajec <matthajec@pm.me>
2025-08-27 13:06:01 -0400

    add hex conversion functions for 0x88 board representation

--- lib/chess_quo/games/rules/chess/notation.ex
+++ lib/chess_quo/games/rules/chess/notation.ex
@@ -15,6 +15,21 @@ defmodule ChessQuo.Games.Rules.Chess.Notation do
     rank_index * 8 + file_index
   end
 
+  def index_to_hex_0x88(index) when is_integer(index) do
+    file = rem(index, 8)
+    rank = div(index, 8)
+    rank * 16 + file
+  end
+
+  def hex_0x88_to_index(square) when is_integer(square) do
+    # 0..7
+    file = rem(square, 16)
+    # 0..7
+    rank = div(square, 16)
+    # 0..63
+    rank * 8 + file
+  end
+
   defp file_to_letter(file) do
     <<?a + file>>
   end
