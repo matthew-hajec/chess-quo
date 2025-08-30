11cac91149479e236cc729ffbf849d6122535062
matthew-hajec <matthajec@pm.me>
2025-08-30 15:19:02 -0400

    add mock ruleset implementation for testing

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -4,3 +4,8 @@ Ecto.Adapters.SQL.Sandbox.mode(ChessQuo.Repo, :manual)
 Mox.defmock(ChessQuo.Games.MockTokens, for: ChessQuo.Games.TokenBehaviour)
 # Use the mock tokens implementation in tests
 Application.put_env(:chess_quo, :tokens, ChessQuo.Games.MockTokens)
+
+# Define the mock for ruleset
+Mox.defmock(ChessQuo.Games.Rules.MockRules, for: ChessQuo.Games.RulesBehaviour)
+# Use the mock ruleset implementations in tests
+Application.put_env(:chess_quo, :ruleset_mods, %{"chess" => ChessQuo.Games.Rules.Chess, "mock" => ChessQuo.Games.Rules.MockRules})
