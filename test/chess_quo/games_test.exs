fbd28b5090a3ac92f206d248f6150d740a3e50bc
matthew-hajec <matthajec@pm.me>
2025-08-30 17:07:17 -0400

    moved tests into describe block for create_game function

--- test/chess_quo/games_test.exs
+++ test/chess_quo/games_test.exs
@@ -22,78 +22,80 @@ defmodule ChessQuo.GamesTest do
     :ok
   end
 
-  test "can insert and fetch a game" do
-    game = GamesFixtures.game_fixture()
-    assert game == Repo.get(Game, game.id)
-  end
+  describe "create_game" do
+    test "can insert and fetch a game" do
+      game = GamesFixtures.game_fixture()
+      assert game == Repo.get(Game, game.id)
+    end
 
-  test "create_game initializes in the waiting state" do
-    assert {:ok, game} = ChessQuo.Games.create_game("mock", "white")
-    assert game.state == "waiting"
-  end
+    test "create_game initializes in the waiting state" do
+      assert {:ok, game} = ChessQuo.Games.create_game("mock", "white")
+      assert game.state == "waiting"
+    end
 
-  test "create_game initializes white as the current turn" do
-    assert {:ok, game} = ChessQuo.Games.create_game("mock", "white")
-    assert game.turn == "white"
-  end
+    test "create_game initializes white as the current turn" do
+      assert {:ok, game} = ChessQuo.Games.create_game("mock", "white")
+      assert game.turn == "white"
+    end
 
-  test "create_game initializes the winner as nil" do
-    assert {:ok, game} = ChessQuo.Games.create_game("mock", "white")
-    assert game.winner == nil
-  end
+    test "create_game initializes the winner as nil" do
+      assert {:ok, game} = ChessQuo.Games.create_game("mock", "white")
+      assert game.winner == nil
+    end
 
-  test "create_game initializes started at as nil" do
-    assert {:ok, game} = ChessQuo.Games.create_game("mock", "white")
-    assert game.started_at == nil
-  end
+    test "create_game initializes started at as nil" do
+      assert {:ok, game} = ChessQuo.Games.create_game("mock", "white")
+      assert game.started_at == nil
+    end
 
-  test "create_game sets code and secrets from tokens module" do
-    ChessQuo.Games.MockTokens
-    |> expect(:game_code, fn -> "TESTCODE" end)
-    |> expect(:secret, fn -> "SECRETONE" end)
-    |> expect(:secret, fn -> "SECRETTWO" end)
+    test "create_game sets code and secrets from tokens module" do
+      ChessQuo.Games.MockTokens
+      |> expect(:game_code, fn -> "TESTCODE" end)
+      |> expect(:secret, fn -> "SECRETONE" end)
+      |> expect(:secret, fn -> "SECRETTWO" end)
 
-    assert {:ok, game} = ChessQuo.Games.create_game("mock", "white")
-    assert game.code == "TESTCODE"
-    assert "SECRETONE" in [game.white_secret, game.black_secret]
-    assert "SECRETTWO" in [game.white_secret, game.black_secret]
-  end
+      assert {:ok, game} = ChessQuo.Games.create_game("mock", "white")
+      assert game.code == "TESTCODE"
+      assert "SECRETONE" in [game.white_secret, game.black_secret]
+      assert "SECRETTWO" in [game.white_secret, game.black_secret]
+    end
 
-  test "create_game returns an error tuple when all attempts yield duplicate codes" do
-    # Create a game with a duplicate code
-    GamesFixtures.game_fixture(%{code: "COPY00"})
+    test "create_game returns an error tuple when all attempts yield duplicate codes" do
+      # Create a game with a duplicate code
+      GamesFixtures.game_fixture(%{code: "COPY00"})
 
-    ChessQuo.Games.MockTokens
-    |> expect(:game_code, 3, fn -> "COPY00" end)
+      ChessQuo.Games.MockTokens
+      |> expect(:game_code, 3, fn -> "COPY00" end)
 
-    # Should return an error tuple
-    assert {:error, _changeset} = ChessQuo.Games.create_game("mock", "white", "", 3)
-  end
+      # Should return an error tuple
+      assert {:error, _changeset} = ChessQuo.Games.create_game("mock", "white", "", 3)
+    end
 
-  test "create_game sets the host color as joined" do
-    ChessQuo.Games.MockTokens
-    |> expect(:game_code, fn -> "111111" end)
-    |> expect(:game_code, fn -> "222222" end)
+    test "create_game sets the host color as joined" do
+      ChessQuo.Games.MockTokens
+      |> expect(:game_code, fn -> "111111" end)
+      |> expect(:game_code, fn -> "222222" end)
 
-    assert {:ok, game} = ChessQuo.Games.create_game("mock", "white")
-    assert game.white_joined
-    refute game.black_joined
+      assert {:ok, game} = ChessQuo.Games.create_game("mock", "white")
+      assert game.white_joined
+      refute game.black_joined
 
-    assert {:ok, game} = ChessQuo.Games.create_game("mock", "black")
-    assert game.black_joined
-    refute game.white_joined
-  end
+      assert {:ok, game} = ChessQuo.Games.create_game("mock", "black")
+      assert game.black_joined
+      refute game.white_joined
+    end
 
-  test "create_game initializes the game using the provided ruleset" do
-    mock_board = [%{"piece" => "pawn", "color" => "white", "position" => 1}]
-    mock_meta = %{"initial" => "meta"}
+    test "create_game initializes the game using the provided ruleset" do
+      mock_board = [%{"piece" => "pawn", "color" => "white", "position" => 1}]
+      mock_meta = %{"initial" => "meta"}
 
-    ChessQuo.Games.Rules.MockRules
-    |> expect(:initial_board, fn -> mock_board end)
-    |> expect(:initial_meta, fn -> mock_meta end)
+      ChessQuo.Games.Rules.MockRules
+      |> expect(:initial_board, fn -> mock_board end)
+      |> expect(:initial_meta, fn -> mock_meta end)
 
-    assert {:ok, game} = ChessQuo.Games.create_game("mock", "white")
-    assert game.board == mock_board
-    assert game.meta == mock_meta
+      assert {:ok, game} = ChessQuo.Games.create_game("mock", "white")
+      assert game.board == mock_board
+      assert game.meta == mock_meta
+    end
   end
 end
