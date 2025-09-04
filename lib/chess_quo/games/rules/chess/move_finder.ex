8bae2c03061016da3d1c2772555cddc05a4b7121
matthew-hajec <matthajec@pm.me>
2025-09-04 11:55:15 -0400

    make to_chess_quo_move private

--- lib/chess_quo/games/rules/chess/move_finder.ex
+++ lib/chess_quo/games/rules/chess/move_finder.ex
@@ -38,7 +38,7 @@ defmodule ChessQuo.Games.Rules.Chess.MoveFinder do
     Enum.map(cl_moves, &to_chess_quo_move/1)
   end
 
-  def to_chess_quo_move(%ChessLogic.Move{
+  defp to_chess_quo_move(%ChessLogic.Move{
         from: %ChessLogic.Piece{type: from_type, color: from_color, square: from_sq},
         to: %ChessLogic.Piece{type: to_type, color: to_color, square: to_sq}
       }) do
