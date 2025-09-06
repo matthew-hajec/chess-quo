f34aa5d712d690a234c90f6cc603e49896738dcf
matthew-hajec <matthajec@pm.me>
2025-09-06 17:40:00 -0400

    fix component not updating due to unreferenced field

--- lib/chess_quo_web/components/game_compnents.ex
+++ lib/chess_quo_web/components/game_compnents.ex
@@ -7,31 +7,8 @@ defmodule ChessQuoWeb.GameComponents do
   attr :ruleset, :string
 
   def icon(%{ruleset: "chess"} = assigns) do
-    piece_svgs = %{
-      "pawn" => %{black: "wikimedia/pawn-black.svg", white: "wikimedia/pawn-white.svg"},
-      "rook" => %{black: "wikimedia/rook-black.svg", white: "wikimedia/rook-white.svg"},
-      "knight" => %{
-        black: "wikimedia/knight-black.svg",
-        white: "wikimedia/knight-white.svg"
-      },
-      "bishop" => %{
-        black: "wikimedia/bishop-black.svg",
-        white: "wikimedia/bishop-white.svg"
-      },
-      "queen" => %{black: "wikimedia/queen-black.svg", white: "wikimedia/queen-white.svg"},
-      "king" => %{black: "wikimedia/king-black.svg", white: "wikimedia/king-white.svg"}
-    }
-
-    assigns =
-      assigns
-      |> Map.put(
-        :svg_path,
-        "/images/chess-pieces/" <> piece_svgs[assigns.piece.type][assigns.piece.color]
-      )
-      |> Map.put(:alt, "#{assigns.piece.color} #{assigns.piece.type}")
-
     ~H"""
-    <img src={@svg_path} alt={@alt} class="w-3/4 h-3/4" draggable="false" />
+    <img src={piece_src(@piece)} class="w-3/4 h-3/4" draggable="false" />
     """
   end
 
@@ -110,6 +87,24 @@ defmodule ChessQuoWeb.GameComponents do
       </div>
     </div>
     """
+  end
+
+  defp piece_src(piece) do
+    "/images/chess-pieces/" <>
+      case {piece.type, piece.color} do
+        {"pawn", :white} -> "wikimedia/pawn-white.svg"
+        {"rook", :white} -> "wikimedia/rook-white.svg"
+        {"knight", :white} -> "wikimedia/knight-white.svg"
+        {"bishop", :white} -> "wikimedia/bishop-white.svg"
+        {"queen", :white} -> "wikimedia/queen-white.svg"
+        {"king", :white} -> "wikimedia/king-white.svg"
+        {"pawn", :black} -> "wikimedia/pawn-black.svg"
+        {"rook", :black} -> "wikimedia/rook-black.svg"
+        {"knight", :black} -> "wikimedia/knight-black.svg"
+        {"bishop", :black} -> "wikimedia/bishop-black.svg"
+        {"queen", :black} -> "wikimedia/queen-black.svg"
+        {"king", :black} -> "wikimedia/king-black.svg"
+      end
   end
 
   defp find_piece_at(index, board_state) do
